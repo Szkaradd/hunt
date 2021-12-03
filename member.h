78c5166ca7c56c833500c8a5b6fd4f7e4d015ac6
Wiktor Grzankowski <wg429211@students.mimuw.edu.pl>
2021-12-03 12:55:16 +0100

    Pierwsze proby member i treasure

--- member.h
+++ member.h
@@ -4,5 +4,40 @@
 
 #ifndef _MEMBER_H
 #define _MEMBER_H
+using strength_t = uint32_t;
+template<typename T, bool trap>
+class Adventurer {
+    strength_t strength;
+    T treasureGathered; // nie wiem jak to przekazac albo pamietac, ale jakos
+public:
+    constexpr static bool isArmed = trap;
+    constexpr Adventurer() {
+//        static_assert(!isArmed, "Is armed!"); // nie wiedziec czemu nie dziala
+        strength = 0;
+        treasureGathered = 0;
+    } // pewnie da sie zgrabniej
+    constexpr Adventurer(strength_t arg)  {
+        static_assert(isArmed == true);
+        static_assert(std::is_integral<T>::value, "Integral required.");
+        strength = arg;
+        treasureGathered = 0;
+    } // naprawde nie wiem jak to ma do konca byc
+
+
+
+    constexpr T getStrength() {
+        static_assert(isArmed == true); // moze cos innego, tylko dla ubzrojonego tasiemca
+        return strength;
+    }
+
+
+    constexpr T pay() {
+        T temp = treasureGathered;
+        treasureGathered = 0;
+        return temp;
+    }
+};
+
 
 #endif //_MEMBER_H
+
--- treasure.h
+++ treasure.h
@@ -4,5 +4,64 @@
 
 #ifndef _TREASURE_H
 #define _TREASURE_H
+// Przykladowe wywolanie
+// Treasure<int, false> t1(5);        Treasure<int, false> t2(6);
+// Treasure<ValueType, IsTrapped>
+// ValueType - całkowitoliczbowy, IsTrapped - logiczny
+
+
+using strength_t = uint32_t;
+
+
+
+//template<template<typename T1> requires Integral<T1> typename T>
+// tu na pewno trzeba dodać odpowiednie constexpr itd, inline itd, ale teraz juz nie ogarniam
+template<typename T,  bool trap> // ale fikoł ja pierdole, bedzie sie psulo jak przekazemy int zamiast bool, potem pomysle
+//requires true<IsTrapped>
+class Treasure {
+    T value;
+public:
+    constexpr Treasure(T arg) {
+        static_assert(std::is_integral<T>::value, "Integral required.");
+        value = arg;
+    }
+    static constexpr bool IsTrapped = trap;
+
+    constexpr T evaluate() {
+        return value;
+    }
+
+    constexpr T getLoot() {
+        T temp = value;
+        value = 0;
+        return temp;
+    }
+
+};
+
+template<typename T>
+class SafeTreasure {
+    T value;
+public:
+    constexpr SafeTreasure(T arg) {
+        static_assert(std::is_integral<T>::value, "Integral required.");
+        value = arg;
+    }
+    constexpr static bool IsTrapped = false;
+
+    constexpr T evaluate() {
+        return value;
+    }
+
+    constexpr T getLoot() {
+        T temp = value;
+        value = 0;
+        return temp;
+    }
+
+};
+
+
 
 #endif //_TREASURE_H
+
