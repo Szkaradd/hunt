2926f7d41d936f3ded9b9c21d0fe30cdd3847125
Mikolaj Szkaradek <ms429606@students.mimuw.edu.pl>
2021-12-04 13:03:37 +0100

    Jest koncept ale on jest jakiś hardy bo lambdusy sa nawalone xd

--- treasure_hunt.h
+++ treasure_hunt.h
@@ -1,7 +1,25 @@
 #ifndef _TREASURE_HUNT_H
 #define _TREASURE_HUNT_H
-using namespace std;
-#include "member.h"
+
 #include "treasure.h"
+#include "member.h"
+#include <concepts>
+#include <utility>
+
+template <typename T>
+// To jest jakieś dziwne nie do końca wiem czy jest ok...
+concept MemberType = requires (T m) {
+    typename T::strength_t;
+
+    {[] () constexpr { return T::isArmed; }() } -> std::convertible_to<bool>;
+
+    {m.pay()} -> std::integral;
+
+    m.loot(std::declval<Treasure<decltype(m.pay()), true>>());
+    m.loot(std::declval<Treasure<decltype(m.pay()), false>>());
+};
+
+template <typename T>
+concept EncounterSide = TreasureType<T> || MemberType<T>;
 
 #endif //_TREASURE_HUNT_H
